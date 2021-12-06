1bc59e60fc6b9131cec9730dc0c3ba27a7bb0b4b
Alvaro Hernandez <alvarohernandezassens@gmail.com>
2021-12-06 20:35:28 +0100

    main requirements working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import InputField from './components/InputField';
 import ToggleSwitch from './components/ToggleSwitch'
 import ResultCard from './components/ResultCard'
-import { useState, useEffect, useReducer } from 'react'
+import { useState, useEffect } from 'react'
 import { Home, Results, Navbar, FormBox } from "./styles/App";
 import React, { Fragment } from 'react';
 import GlobalStyle from './styles/globalStyles';
@@ -10,7 +10,6 @@ import GlobalStyle from './styles/globalStyles';
 function App() {
   // Data coming from API
   const [data, setData] = useState([]);
-
   // component state
   const [excavationValue, setExcavation] = useState(true);
   const [plumbingValue, setPlumbing] = useState(true);
@@ -21,7 +20,7 @@ function App() {
     const fetchData = async () => {
       const data = await fetch("http://localhost:5000/clients");
       const json = await data.json();
-      console.log(json)
+      json.map(el => el.show = true)
       setData(json);
     }
 
@@ -33,7 +32,52 @@ function App() {
     setFilterCompanies(e.target.value)
   };
 
-  const filterResults = function () {
+  const handleCheckbox = target => {
+    let newData;
+    switch (target.id) {
+      case "Excavation":
+        setExcavation(!excavationValue)
+          newData = data.map(el => {
+          if (el.specialty === "Excavation") {
+            el.show = !excavationValue;
+            return el;
+          } else {
+            return el;
+          }
+        })
+        setData(newData);
+        break;
+      case "Plumbing":
+        setPlumbing(!plumbingValue)
+        newData = data.map(el => {
+          if (el.specialty === "Plumbing") {
+            el.show = !plumbingValue;
+            return el;
+          } else {
+            return el;
+          }
+        })
+        setData(newData);
+        break;
+      case "Electrical":
+        setElectrical(!electricalValue)
+        newData = data.map(el => {
+          if (el.specialty === "Electrical") {
+            el.show = !electricalValue;
+            return el;
+          } else {
+            return el;
+          }
+        })
+        setData(newData);
+        break;
+      default:
+        console.error("Error")
+        break;
+    }
+  };
+
+  function filterResults() {
     return data.filter((value) => {
       if (filterCompanies === "") {
         return value;
@@ -55,29 +99,37 @@ function App() {
           <FormBox>
             <InputField placeholder="Filter by company name..." name="company_name" type="text" required={false} handleInput={handleChange}/>
             <div>
-              <ToggleSwitch title="Excavation"
+              <ToggleSwitch
+                title="Excavation"
+                id="Excavation"
                 size="xs"
                 value={excavationValue}
                 checked={excavationValue}
-                onChange={({ target }) => setExcavation(!excavationValue)} />
-              <ToggleSwitch title="Plumbing"
+                onChange={({ target }) => handleCheckbox(target)} />
+              <ToggleSwitch
+                title="Plumbing"
+                id="Plumbing"
                 size="xs"
                 value={plumbingValue}
                 checked={plumbingValue}
-                onChange={({ target }) => setPlumbing(!plumbingValue)} />
-              <ToggleSwitch title="Electrical"
+                onChange={({ target }) => handleCheckbox(target)} />
+              <ToggleSwitch
+                title="Electrical"
+                id="Electrical"
                 size="xs"
                 value={electricalValue}
                 checked={electricalValue}
-                onChange={({ target }) => setElectrical(!electricalValue)} />
+                onChange={({ target }) => handleCheckbox(target)} />
             </div>
           </FormBox>
         </Navbar>
         <Results><p>RESULTS:</p>
           {filterResults().map(
             (el, index) => {
-              const {specialty, city, company_name, logo} = el
-              return <ResultCard key={index} name={company_name} city={city} imgSrc={logo} specialty={specialty} />
+              if (el.show) {
+                const {specialty, city, company_name, logo} = el
+                return <ResultCard key={index} name={company_name} city={city} imgSrc={logo} specialty={specialty} />
+              }
             }
           )}
         </Results>
--- frontend/src/styles/ResultCard.js
+++ frontend/src/styles/ResultCard.js
@@ -1,17 +1,18 @@
 import styled from "styled-components";
 
 export const Box = styled.div`
-  box-shadow: 0 0px 1px rgba(0, 0, 0, 0.10), 0 2px 2px rgba(0, 0, 0, 0.10);
-	background: #fff;
+  box-shadow: 0 0px 1px rgba(0, 0, 0, 0.1), 0 2px 2px rgba(0, 0, 0, 0.1);
+  background: #fff;
   border-radius: 5px;
-  display:flex;
+  display: flex;
   flex-direction: row;
   justify-content: space-between;
-  margin: 10px;
+  margin: 16px;
+  cursor: pointer;
 `;
 
 export const CardBody = styled.div`
-  display:flex;
+  display: flex;
   padding: 30px;
   flex-direction: column;
 `;
@@ -19,10 +20,8 @@ export const CardBody = styled.div`
 export const Image = styled.img`
   background-size: cover;
   background-position: center;
-        background-repeat: no-repeat;
-
-        border-top-right-radius: 5px;
-        border-bottom-right-radius: 5px;
-        overflow:hidden;
-      
+  background-repeat: no-repeat;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  overflow: hidden;
 `;
